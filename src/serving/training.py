c51ab7197fa9b996f725941f5a03d5a439b8dc4a
Côme Arvis <come.arvis@neuronest.net>
2020-08-19 12:11:18 +0200

    fix: resolve iteration offset issue

--- src/serving/training.py
+++ src/serving/training.py
@@ -28,6 +28,8 @@ async def post(
     tensorboard_path = os.path.join(run_path, ConfigPath.tensorboard_endpath)
     os.makedirs(iteration_path, exist_ok=True)
     os.makedirs(tensorboard_path, exist_ok=True)
+    updated = False
+    request.app.state.number_samples += len(states)
     writer = tf.summary.create_file_writer(tensorboard_path)
     loss = train(
         model,
@@ -36,13 +38,6 @@ async def post(
         epochs=ConfigServing.training_epochs,
         batch_size=ConfigServing.batch_size,
     )
-    response = {
-        "loss": loss,
-        "updated": False,
-        "iteration": request.app.state.iteration,
-    }
-    request.app.state.iteration += 1
-    request.app.state.number_samples += len(states)
     with writer.as_default():
         tf.summary.scalar("loss", loss, step=request.app.state.iteration)
         tf.summary.scalar(
@@ -74,7 +69,7 @@ async def post(
                 // ConfigServing.model_checkpoint_frequency,
             )
             writer.flush()
-        response["updated"] = score >= ConfigServing.replace_min_score
+        updated = score >= ConfigServing.replace_min_score
     if request.app.state.iteration % ConfigServing.samples_checkpoint_frequency == 0:
         print("Saving current samples...")
         np.savez(
@@ -83,4 +78,10 @@ async def post(
             policies=policies,
             values=values,
         )
+    response = {
+        "loss": loss,
+        "updated": updated,
+        "iteration": request.app.state.iteration,
+    }
+    request.app.state.iteration += 1
     return response
