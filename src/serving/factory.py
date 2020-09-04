a5b962f8f406831620693e72c5d09fbfccdf880a
Côme Arvis <come.arvis@neuronest.net>
2020-09-04 18:35:22 +0200

    feat: HTTP requests implemented in factory

--- src/serving/factory.py
+++ src/serving/factory.py
@@ -2,7 +2,7 @@ import requests
 import json
 import uuid
 import numpy as np
-from typing import Tuple, List
+from typing import Tuple, Optional
 
 from src.config import ConfigGeneral, ConfigServing, ConfigPath
 
@@ -55,25 +55,63 @@ def infer_sample(state: np.ndarray, concurrency: bool) -> Tuple[np.ndarray, floa
     return np.asarray(response_content["probabilities"]), response_content["value"]
 
 
-def train_run_samples_post(
-    run_id: str, states: np.ndarray, labels: List[np.ndarray]
-) -> Tuple[float, bool, int]:
+def get_run_id() -> Optional[str]:
+    response = requests.get(url=ConfigServing.serving_address + ConfigPath.run_id_path,)
+    try:
+        response_content = json.loads(response.content)
+    except json.decoder.JSONDecodeError:
+        return None
+    return response_content.get("run_id")
+
+
+def append_queue(states: np.ndarray, policies: np.ndarray, values: np.ndarray):
     headers = {"content-type": "application/octet-stream"}
-    policies, values = labels
     data = {
-        "run_id": run_id,
         "states": states.tolist(),
         "policies": policies.tolist(),
         "values": values.tolist(),
     }
-    response = requests.post(
-        url=ConfigServing.serving_address + ConfigPath.training_path,
+    requests.patch(
+        url=ConfigServing.serving_address + ConfigPath.append_queue_path,
         data=json.dumps(data),
         headers=headers,
     )
-    response_content = json.loads(response.content)
-    return (
-        response_content["loss"],
-        response_content["updated"],
-        response_content["iteration"],
+
+
+def retrieve_queue() -> Optional[Tuple[np.ndarray, np.ndarray, np.ndarray]]:
+    headers = {"content-type": "application/octet-stream"}
+    response = requests.put(
+        url=ConfigServing.serving_address + ConfigPath.retrieve_queue_path,
+        data={},
+        headers=headers,
     )
+    try:
+        response_content = json.loads(response.content)
+    except json.decoder.JSONDecodeError:
+        return None
+    states, policies, values = (
+        np.asarray(response_content.get("states")),
+        np.asarray(response_content.get("policies")),
+        np.asarray(response_content.get("values")),
+    )
+    return states, policies, values
+
+
+def update_best_model():
+    headers = {"content-type": "application/octet-stream"}
+    requests.put(
+        url=ConfigServing.serving_address + ConfigPath.update_best_model_path,
+        data={},
+        headers=headers,
+    )
+
+
+def get_queue_size() -> Optional[int]:
+    response = requests.get(
+        url=ConfigServing.serving_address + ConfigPath.size_queue_path,
+    )
+    try:
+        response_content = json.loads(response.content)
+    except json.decoder.JSONDecodeError:
+        return None
+    return response_content.get("queue_size")
