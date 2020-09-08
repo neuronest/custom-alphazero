48cb858d7e2504f8eb1c865a9a0b9ba865786e32
yohskua <guillaume.ligner@neuronest.net>
2020-09-08 18:13:34 +0200

    style: rename residual_connexion layer

--- src/model/tensorflow/base_layers.py
+++ src/model/tensorflow/base_layers.py
@@ -104,7 +104,7 @@ class OuterConvBlock(Layer):
             l2_penalization_term=l2_penalization_term,
         )
         if self.residual_connexion:
-            self.residual_connexion = InnerConvBlock(
+            self.residual_connexion_layer = InnerConvBlock(
                 input_dim=input_dim,
                 filters=filters,
                 kernel_size=(1, 1),
@@ -116,7 +116,7 @@ class OuterConvBlock(Layer):
             )
             self.add_residual = Add()
         else:
-            self.residual_connexion, self.add_residual = None, None
+            self.residual_connexion_layer, self.add_residual = None, None
         self.activation_layer = Activation(self.activation)
 
     @tf.function
@@ -125,7 +125,7 @@ class OuterConvBlock(Layer):
         outputs = self.inner_conv_1(outputs)
         outputs = self.inner_conv_2(outputs)
         if self.residual_connexion:
-            identity_resized = self.residual_connexion(inputs)
+            identity_resized = self.residual_connexion_layer(inputs)
             outputs = self.add_residual([identity_resized, outputs])
         outputs = self.activation_layer(outputs)
         return outputs
